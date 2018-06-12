fa0bf0baf9c66233479c46a44ffdf0dbf06da623
Jay Lee <busyjaylee@gmail.com>
2018-06-12 12:40:29 +0800

    revert is_row_key

--- src/raftstore/coprocessor/split_observer.rs
+++ src/raftstore/coprocessor/split_observer.rs
@@ -38,9 +38,15 @@ impl SplitObserver {
                 Err(_) => continue,
             };
 
-            if table::is_row_key(&key) {
-                // row key, truncate to row key itself.
-                key.truncate(table::RECORD_ROW_KEY_LEN);
+            // format of a key is TABLE_PREFIX + table_id + RECORD_PREFIX_SEP + handle + column_id
+            // + version or TABLE_PREFIX + table_id + INDEX_PREFIX_SEP + index_id + values + version
+            // or meta_key + version
+            // The length of TABLE_PREFIX + table_id is TABLE_PREFIX_KEY_LEN.
+            if key.starts_with(table::TABLE_PREFIX) && key.len() > table::TABLE_PREFIX_KEY_LEN
+                && key[table::TABLE_PREFIX_KEY_LEN..].starts_with(table::RECORD_PREFIX_SEP)
+            {
+                // row key, truncate to handle
+                key.truncate(table::PREFIX_LEN + table::ID_LEN);
             }
 
             let key = encode_bytes(&key);
