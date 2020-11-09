156df7fb88e469531cda73448b13f3930763581f
Jay Lee <BusyJayLee@gmail.com>
2020-11-09 21:50:19 +0800

    remove trace from signal
    Signed-off-by: Jay Lee <BusyJayLee@gmail.com>

--- cmd/src/signal_handler.rs
+++ cmd/src/signal_handler.rs
@@ -9,69 +9,7 @@ mod imp {
     use libc::c_int;
     use nix::sys::signal::{SIGHUP, SIGINT, SIGTERM, SIGUSR1, SIGUSR2};
     use signal::trap::Trap;
-    use std::fmt::Write;
-    use std::fs::File;
-    use std::{env, error, fmt};
     use tikv_util::metrics;
-
-    fn fill_flamegraph(
-        buffer: &mut String,
-        trace: &mut String,
-        snapshot: &tikv_alloc::trace::MemoryTrace,
-    ) -> fmt::Result {
-        let origin_len = trace.len();
-        if trace.is_empty() {
-            write!(trace, "{}", snapshot.id())?;
-        } else {
-            write!(trace, ";{}", snapshot.id())?;
-        }
-        let mut total = 0;
-        append_readable_size(trace, snapshot.size());
-        for c in snapshot.children() {
-            fill_flamegraph(buffer, trace, c)?;
-            total += c.size();
-        }
-        // When merging frame, size will of children will also be counted.
-        if snapshot.size() > total {
-            write!(buffer, "{} {}\n", trace, snapshot.size() - total)?;
-        }
-        trace.truncate(origin_len);
-        Ok(())
-    }
-
-    fn append_readable_size(s: &mut String, size: usize) {
-        let units = &["", "KiB", "MiB", "GiB"];
-        let mut size = size as f64;
-        for unit in units {
-            if size > 1024.0 {
-                size /= 1024.0;
-                continue;
-            }
-            write!(s, "-{:.1}{}", size, unit).unwrap();
-            return;
-        }
-        write!(s, " {:.1}TiB", size).unwrap();
-    }
-
-    fn generate_flamegraph(
-        snapshot: &tikv_alloc::trace::MemoryTrace,
-    ) -> Result<(), Box<dyn error::Error>> {
-        let mut buffer = String::new();
-        let mut trace = String::new();
-        fill_flamegraph(&mut buffer, &mut trace, &snapshot)?;
-
-        let path = env::temp_dir().join("tikv_memory.svg");
-        let out = File::create(&path)?;
-        info!("{}", buffer);
-        inferno::flamegraph::from_lines(
-            &mut inferno::flamegraph::Options::default(),
-            buffer.lines(),
-            out,
-        )?;
-
-        info!("memory flamegraph is generated at {}", path.display());
-        Ok(())
-    }
 
     #[allow(dead_code)]
     pub fn wait_for_signal<ER: RaftEngine>(engines: Option<Engines<RocksEngine, ER>>) {
@@ -88,10 +26,6 @@ mod imp {
                     if let Some(ref engines) = engines {
                         info!("{:?}", MiscExt::dump_stats(&engines.kv));
                         info!("{:?}", RaftEngine::dump_stats(&engines.raft));
-                    }
-
-                    if let Err(e) = generate_flamegraph(&tikv_alloc::trace::snapshot()) {
-                        error!("failed to generate flamegraph: {:?}", e);
                     }
                 }
                 // TODO: handle more signal
