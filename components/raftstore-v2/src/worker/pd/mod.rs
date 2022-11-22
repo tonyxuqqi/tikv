a05fb21e7ef47206ac0736ef92bdabe7bc0bda58
tabokie <xy.tao@outlook.com>
2022-11-22 17:01:15 +0800

    handle heartbeat response
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- components/raftstore-v2/src/worker/pd/mod.rs
+++ components/raftstore-v2/src/worker/pd/mod.rs
@@ -98,6 +98,7 @@ where
     ER: RaftEngine,
     T: PdClient + 'static,
 {
+    store_id: u64,
     pd_client: Arc<T>,
     raft_engine: ER,
     tablet_factory: Arc<dyn TabletFactory<EK>>,
@@ -111,6 +112,7 @@ where
     store_stat: store_heartbeat::StoreStat,
 
     region_cpu_records: HashMap<u64, u32>,
+    is_hb_receiver_scheduled: bool,
 
     concurrency_manager: ConcurrencyManager,
 
@@ -126,6 +128,7 @@ where
     T: PdClient + 'static,
 {
     pub fn new(
+        store_id: u64,
         pd_client: Arc<T>,
         raft_engine: ER,
         tablet_factory: Arc<dyn TabletFactory<EK>>,
@@ -136,6 +139,7 @@ where
         logger: Logger,
     ) -> Self {
         Self {
+            store_id,
             pd_client,
             raft_engine,
             tablet_factory,
@@ -145,6 +149,7 @@ where
             start_ts: UnixSecs::zero(),
             store_stat: store_heartbeat::StoreStat::default(),
             region_cpu_records: HashMap::default(),
+            is_hb_receiver_scheduled: false,
             concurrency_manager,
             causal_ts_provider,
             logger,
@@ -160,6 +165,7 @@ where
     type Task = Task;
 
     fn run(&mut self, task: Task) {
+        self.maybe_schedule_heartbeat_receiver();
         match task {
             Task::Heartbeat(task) => self.handle_heartbeat(task),
             Task::StoreHeartbeat { stats } => self.handle_store_heartbeat(stats),
